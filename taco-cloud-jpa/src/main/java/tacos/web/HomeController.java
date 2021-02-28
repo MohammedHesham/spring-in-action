671a7268c507bf79cfeb6c3afcee4ccaca117a3c
mhesham <mhesham>
2021-02-28 14:20:42 +0200

    edit security config

---
+++ taco-cloud-jpa/src/main/java/tacos/web/HomeController.java
@@ -0,0 +1,17 @@
+package tacos.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	@GetMapping("/")
+	public String home()
+	{
+		return "registration";
+	}
+}
